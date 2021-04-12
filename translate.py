edf89ebc67c4ec5b880ced4e0c015a5757815b21
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-12 16:39:24 -0400

    create translation function

--- translate.py
+++ translate.py
@@ -6,6 +6,16 @@ input_text = 'eel'
 with open('dictionary.json', 'r') as dictionary_file:
     dictionary = json.loads(dictionary_file.read())
 
+def translate_word(word):
+    return next(filter(lambda x: x['english'] == word, dictionary))['translation']
+
+def translate(text):
+    input_words = text.split(' ')
+    output = ''
+    for word in input_words:
+        output += translate_word(word)
+    return output
+
 # Build dictionary
 for term in dictionary:
     # Generate compound terms from translations of parts (e.g., eel --> water + snake)
